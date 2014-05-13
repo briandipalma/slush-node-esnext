cdcdf42b88204cd597b6c4fe44aa574067cd8e5d
Brian Di Palma <offler@gmail.com>
2014-05-13 13:07:08 +0100

    Add a "bin" subgenerator.
    Add bin subgenerator which is used to create bin package boilerplate.
    Includes a bin_template directory with a bin script.

---
+++ bin_templates/bin/package-cli.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+"use strict";
+
+var <%= packageName %> = require("../");
+
+//process.argv - Array that contains the command line arguments.
--- slushfile.js
+++ slushfile.js
@@ -4,6 +4,7 @@ var path = require("path");
 
 var gulp = require("gulp");
 var inquirer = require("inquirer");
+var rename = require("gulp-rename");
 var install = require("gulp-install");
 var template = require("gulp-template");
 var conflict = require("gulp-conflict");
@@ -16,6 +17,29 @@ var prompts = [{
 }];
 
 var templatesDirectory = path.join(__dirname, "templates", "**");
+var binTemplatesDirectory = path.join(__dirname, "bin_templates", "**");
+
+function renameBinFile(packageName, file) {
+	if (file.basename === "package-cli") {
+		file.basename = packageName + "-cli";
+	}
+}
+
+function binTask(done) {
+	var packageJSONPath = path.join(process.cwd(), "package");
+	var packageJSON = require(packageJSONPath);
+	var packageName = packageJSON.name;
+	var templateData = {packageName: packageName};
+
+	gulp.src(binTemplatesDirectory)
+		.pipe(template(templateData))
+		.pipe(rename(renameBinFile.bind(null, packageName)))
+		.pipe(conflict("./"))
+		.pipe(gulp.dest("./"))
+		.on("end", function() {
+			done();
+		});
+}
 
 function questionsAnswered(done, answers) {
 	gulp.src(templatesDirectory)
@@ -28,6 +52,9 @@ function questionsAnswered(done, answers) {
 		});
 }
 
-gulp.task("default", function(done) {
+function defaultTask(done) {
 	inquirer.prompt(prompts, questionsAnswered.bind(null, done));
-});
+}
+
+gulp.task("bin", binTask);
+gulp.task("default", defaultTask);
